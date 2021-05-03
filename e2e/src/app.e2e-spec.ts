8ef441a333212fe609f667a9c764484b64b3d864
Tasfin Jayed <tasfin.jayed1220@gmail.com>
2021-05-03 15:27:45 +0600

    Rename the project.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('demo-dashboard-ui app is running!');
+    expect(await page.getTitleText()).toEqual('demo-application-ui app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/demo-dashboard-ui'),
+      dir: require('path').join(__dirname, './coverage/demo-application-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
